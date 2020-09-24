4f6a0c119480df7694af32f631fe5fc2935795f0
Isabbelle <63454904+Isabbelle@users.noreply.github.com>
2020-09-24 15:22:23 +0200

    Add files via upload

---
+++ first.js
@@ -0,0 +1,35 @@
+console.log("Hello class")
+
+function onButtonClick() {
+    alert("you did it");
+    console.log("You did it");
+}
+
+
+
+function onButtonClick() {
+
+    document.body.style.backgroundColor = "grey";
+    
+}
+
+function changeButtonOne() {
+
+    document.body.style.backgroundColor = "Pink";
+    
+    
+}
+
+function changeButtonTwo() {
+
+    document.body.style.backgroundColor = "Green";
+}
+
+function changeButtonThree() {
+
+    document.body.style.backgroundColor = "Blue";
+}
+
+console.log('blah');
+console.log('blah');
+console.log('blah');
